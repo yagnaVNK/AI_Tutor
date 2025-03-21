0c78ab03f41be4c5b3946356084b2728c26f9374
yagnaVNK <yagna.kaasaragadda@gmail.com>
2025-03-20 22:14:04 -0400

    inital commit

---
+++ app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import ollama
+
+def call_llm(prompt, model="llama3:8b", chat_history=[]):
+    try:
+        system_prompt = "You are a funny helpful ai assistant who gives the answers in a short and conversational way."
+        messages = [{"role": "system", "content": system_prompt}]
+        messages.extend([{"role": role, "content": content} for role, content in chat_history])
+        messages.append({"role": "user", "content": prompt})
+
+        response = ollama.chat(
+            model=model,
+            messages=messages
+        )
+        return response["message"]["content"]
+    except Exception as e:
+        return f"Error: {e}"
+
+def get_available_models():
+    try:
+        models = ollama.list_models()
+        return [model['name'] for model in models]
+    except Exception as e:
+        return [f"Error fetching models: {e}"]
+
+def main():
+    st.set_page_config(page_title="LLM Chatbot using Ollama", layout="centered")
+    st.title("🤖 Chatbot using Ollama LLaMA")
+
+    st.sidebar.title("Settings")
+    models = ["llama3:8b","llama3.2:1b"]
+    model = st.sidebar.selectbox("Choose LLaMA Model", models)
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+
+    for chat in st.session_state.chat_history:
+        role, content = chat
+        if role == "user":
+            st.chat_message("user").markdown(content)
+        else:
+            st.chat_message("assistant").markdown(content)
+
+    prompt = st.chat_input("Ask something...")
+
+    if prompt:
+        st.chat_message("user").markdown(prompt)
+        st.session_state.chat_history.append(("user", prompt))
+
+        response = call_llm(prompt, model, st.session_state.chat_history)
+        st.chat_message("assistant").markdown(response)
+        st.session_state.chat_history.append(("assistant", response))
+
+if __name__ == "__main__":
+    main()
